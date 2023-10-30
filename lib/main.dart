57282576a53e9ef99bc7a97c38bc6671c2905682
Saniul haque <haquesaniul@gmail.com>
2023-10-30 14:32:06 +0530

    ➕ ➖  use audioplayers instead

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:just_audio/just_audio.dart';
-import 'package:mikikuru/player.dart';
 
 void main() {
   runApp(
@@ -27,9 +26,12 @@ class App extends StatelessWidget {
 
 class AudioFile {
   final PlatformFile file;
-  final AudioSource source;
+  final PlatformFile? cover;
 
-  AudioFile({required this.file, required this.source});
+  AudioFile({
+    required this.file,
+    this.cover,
+  });
 }
 
 class HomePage extends StatefulWidget {
@@ -41,6 +43,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<AudioFile> audioSources = [];
+  final player = AudioPlayer();
 
   @override
   Widget build(BuildContext context) {
@@ -52,13 +55,27 @@ class _HomePageState extends State<HomePage> {
             onPressed: () async {
               FilePickerResult? result = await FilePicker.platform.pickFiles(
                 type: FileType.custom,
-                allowedExtensions: ['mp3', 'm4b'],
+                allowedExtensions: ['mp3', 'm4b', 'jpg', 'png'],
                 allowMultiple: true,
               );
-              final files = (result?.files ?? [])..sort((a, b) => a.name.compareTo(b.name));
+              final audioFiles = result?.files
+                      .where((e) =>
+                          e.path != null && (e.path!.endsWith('.mp3') || e.path!.endsWith('.m4b')))
+                      .toList() ??
+                  [];
+              final coverFiles = result?.files
+                      .where((e) =>
+                          e.path != null && (e.path!.endsWith('.jpg') || e.path!.endsWith('.png')))
+                      .toList() ??
+                  [];
+              final files = audioFiles..sort((a, b) => a.name.compareTo(b.name));
+
               setState(() {
                 audioSources = files
-                    .map((file) => AudioFile(file: file, source: AudioSource.file(file.path ?? '')))
+                    .map((file) => AudioFile(
+                          file: file,
+                          cover: coverFiles.firstOrNull,
+                        ))
                     .toList();
               });
             },
@@ -72,16 +89,14 @@ class _HomePageState extends State<HomePage> {
             shrinkWrap: true,
             itemCount: audioSources.length,
             itemBuilder: (_, i) {
-              final file = audioSources[i];
+              final audio = audioSources[i];
               return ListTile(
-                title: Text(file.file.name),
-                onTap: () => Navigator.of(context).push<void>(
-                  MaterialPageRoute<void>(
-                    builder: (BuildContext context) => AppAudioPlayer(
-                      audioSource: file.source,
-                    ),
-                  ),
-                ),
+                title: Text(audio.file.name),
+                onTap: () async {
+                  if (audio.file.path != null) {
+                    await player.play(DeviceFileSource(audio.file.path!));
+                  }
+                },
               );
             },
           )
