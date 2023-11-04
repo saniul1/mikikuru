aa3ce25f23eb608a37af9f7a28e994a74f85c62f
Saniul haque <haquesaniul@gmail.com>
2023-11-04 22:21:59 +0530

    💄 book hover effect

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mikikuru/states/cover_art_notifier.dart';
 
@@ -58,20 +59,15 @@ class _HomePageState extends State<HomePage> {
               );
               final audioFiles = result?.files
                       .where((e) =>
-                          e.path != null &&
-                          (e.path!.endsWith('.mp3') ||
-                              e.path!.endsWith('.m4b')))
+                          e.path != null && (e.path!.endsWith('.mp3') || e.path!.endsWith('.m4b')))
                       .toList() ??
                   [];
               final coverFiles = result?.files
                       .where((e) =>
-                          e.path != null &&
-                          (e.path!.endsWith('.jpg') ||
-                              e.path!.endsWith('.png')))
+                          e.path != null && (e.path!.endsWith('.jpg') || e.path!.endsWith('.png')))
                       .toList() ??
                   [];
-              final files = audioFiles
-                ..sort((a, b) => a.name.compareTo(b.name));
+              final files = audioFiles..sort((a, b) => a.name.compareTo(b.name));
 
               setState(() {
                 audioSources = files
@@ -120,23 +116,8 @@ class _HomePageState extends State<HomePage> {
               ),
               itemCount: 16,
               itemBuilder: (_, i) {
-                final image =
-                    AssetImage('assets/images/default_cover_${i + 1}.jpeg');
-                return InkWell(
-                  onTap: () {
-                    AudioBookCoverNotifier().value = image;
-                  },
-                  child: Container(
-                    clipBehavior: Clip.antiAlias,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(8)),
-                    ),
-                    child: Image(
-                      image: image,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                );
+                final image = AssetImage('assets/images/default_cover_${i + 1}.jpeg');
+                return BookCover(image: image, i: i);
               },
             ),
           ),
@@ -161,5 +142,153 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+  }
+}
+
+class BookCover extends StatefulWidget {
+  const BookCover({
+    super.key,
+    required this.image,
+    required this.i,
+  });
+
+  final AssetImage image;
+  final int i;
+
+  @override
+  _BookCoverState createState() => _BookCoverState();
+}
+
+class _BookCoverState extends State<BookCover> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _animation;
+  bool isHover = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 100),
+      vsync: this,
+    );
+
+    _animation = Tween<double>(begin: 1, end: 1.1).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onHover: (hover) {
+        hover ? _controller.forward() : _controller.reverse();
+        setState(() {
+          isHover = hover;
+        });
+      },
+      onTap: () {
+        AudioBookCoverNotifier().value = widget.image;
+      },
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: AnimatedBuilder(
+              animation: _animation,
+              builder: (context, child) {
+                return Transform.scale(
+                  scale: _animation.value,
+                  child: Image(
+                    image: widget.image,
+                    fit: BoxFit.cover,
+                  ),
+                );
+              },
+            ),
+          ),
+          if (isHover)
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 1,
+                    offset: const Offset(0, 0), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Sit amet do dolore dolor Duis labore nulla reprehenderit anim.',
+                      softWrap: true,
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Colors.white),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          '03:44:32',
+                          style: Theme.of(context).textTheme.labelMedium,
+                        ),
+                        Container(
+                          decoration: const BoxDecoration(
+                            color: Colors.white30,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  widget.i != 3
+                                      ? CupertinoIcons.play_arrow_solid
+                                      : CupertinoIcons.pause_solid,
+                                  size: 12,
+                                ),
+                                const SizedBox(width: 4),
+                                Text(
+                                  widget.i == 3
+                                      ? 'Playing'
+                                      : [0, 2, 8, 9].contains(widget.i)
+                                          ? 'Continue'
+                                          : 'Play Now',
+                                  style: Theme.of(context).textTheme.labelMedium,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
